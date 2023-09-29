4e737a244945cbd4f5e2e43f1925bbd23cd49c71
C00287604@setu.ie <C00287604@setu.ie>
2023-09-29 10:02:51 +0100

    Q1

---
+++ lab2 Question/Lab21q1.cpp
@@ -0,0 +1,14 @@
+#include "Triangle.h"
+#include <iostream>
+
+int main()
+{
+	Triangle t1(0, 0);
+	std::cout << "t1 area = " << t1.area() << std::endl;;
+    
+	std::cout << "t2 hypo = " << t1.hypot() << std::endl; 
+
+	
+
+
+}
---
+++ lab2 Question/Triangle.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+
+
+
+class Triangle
+{
+public:
+    Triangle() = default;
+    Triangle(float t_base, float t_height) : m_base(t_base), m_height(t_height)
+    {}
+    
+
+    float hypot()
+    {
+        
+
+
+
+        return 0.0f;
+    }
+
+    float area()
+    {
+
+        return 0.0f;
+    }
+
+
+private:
+    float m_base{12};
+    float m_height{ 5 };
+
+};
